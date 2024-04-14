5a2c14e4360559069b8987b6eaa4c03c998e8c74
Yuval Uner <yuvaluner@gmail.com>
2024-04-14 16:25:31 +0300

    Got auto optimization working for weighted_combination.py

--- filter_methods/weighted_combination.py
+++ filter_methods/weighted_combination.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+import sklearn.linear_model
 from sklearn.feature_selection import mutual_info_regression, mutual_info_classif
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import mean_squared_error
 
 
 class WeightedCombination:
@@ -15,6 +17,10 @@ class WeightedCombination:
         self.correlation = None
         self.correlation_weight = correlation_weight
         self.target_column = None
+
+
+    def set_weight(self, correlation_weight):
+        self.correlation_weight = correlation_weight
 
     def fit(self, X, target_column=None, continuous_cols=None, categorical_cols=None):
         if target_column is None and self.target_column is None:
@@ -54,37 +60,117 @@ class WeightedCombination:
             mutual_information = dict(zip(data.columns, mutual_info_regression(data, y)))
             self.mutual_information = pd.Series(mutual_information)
 
-    def transform(self, X, num_features=0, min_score=0):
+    def transform(self, X, num_features=0, min_threshold=0):
         """
         :param X:
         :param num_features:
-        :param min_score:
+        :param min_threshold:
         :return: The dataframe with only the selected features, the list of selected features and the feature scores
         """
-        if num_features == 0 and min_score == 0:
-            raise ValueError("Either num_features or min_score must be set")
+        if num_features == 0 and min_threshold == 0:
+            raise ValueError("Either num_features or min_threshold must be set")
         # Compute the feature scores series
         feature_scores = self.correlation_weight * self.correlation.values + \
                          (1 - self.correlation_weight) * self.mutual_information
         # Sort the features by their scores
         feature_scores = feature_scores.sort_values(ascending=False)
-        possible_features = feature_scores[feature_scores >= min_score]
+        possible_features = feature_scores[feature_scores >= min_threshold]
         if num_features > 0:
-            feature_list = possible_features.head(num_features).index
+            try:
+                feature_list = possible_features.head(num_features).index
+            # If the number of features is greater than the number of features with a score above the threshold
+            # we will select all the features with a score above the threshold
+            except ValueError:
+                feature_list = possible_features.index
         else:
             feature_list = possible_features.index
         return X[feature_list], feature_list, feature_scores
 
-    def auto_optimize(self, X_train, X_test, y_train, y_test, model: LinearRegression, num_features=0):
+    def test_on_values(self, X_train, y_train, X_test, y_test,
+                       model, num_features, parameter_value, loss_function, threshold=0):
+        """
+        Tests the performance of the model on the selected features.
+        :param X_train:
+        :param y_train:
+        :param X_test:
+        :param y_test:
+        :param model:
+        :return: the model's performance
+        """
+        # Copy the uninitialized model
+        model = model.__class__()
+        # Set the correlation weight to the parameter value
+        self.correlation_weight = parameter_value
+        # Get the selected features
+        X_train, selected_features, _ = self.transform(X_train, num_features=num_features, min_threshold=threshold)
+        if len(selected_features) == 0:
+            return np.inf
+        X_test = X_test[selected_features]
+        # Fit the model and get the predictions
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+        # Return the model's performance
+        return loss_function(y_test, preds)
+
+    def auto_optimize(self, X_train, y_train, X_test, y_test,
+                      model: sklearn.linear_model = sklearn.linear_model.LinearRegression(),
+                      loss_function: callable = mean_squared_error,
+                      include_threshold_optimization=False):
         """
         Optimizes the weights for the correlation and information gain, by maximizing the model's performance.
-        Optimal weights are found using a binary search.
-        :param X:
-        :param y:
-        :param model:
-        :return: a dataframe with the selected features
+        Optimal weights are found using a grid search on the correlation weight and number of features. :param X:
+        :param X_train:
+        :param y_train:
+        :param X_test:
+        :param y_test:
+        :param model: A model to be used for the optimization. Must have a fit and predict method. Default
+        is LinearRegression. For custom models, make sure the model class has the fit function,
+        with pre-defined hyperparameters.
+        :param loss_function: A loss function to be used for the optimization. Default is
+        mean_squared_error.
+        :param include_threshold_optimization: A boolean indicating whether to include the optimization of the
+        min_threshold parameter. Default is False. Note that this will significantly increase the time taken,
+        as it will perform a 3d grid search instead of a 2d grid search.
+        :return: a dataframe with the selected features, the list of selected features and the feature scores,
+        the best weight, the best loss, the best number of features and the best threshold found
         """
-        best_score = 0
-        model_copy = model
-        self.correlation_weight = 1
-        self.information_gain_weight = 0
+        # Initialize the best loss and weight
+        best_loss = np.inf
+        best_weight = None
+        best_num_features = None
+        best_threshold = 0
+        # Initialize the grid search parameters
+        correlation_weights = np.linspace(0, 1, 101)
+        num_features = np.arange(1, len(X_train.columns) + 1)
+        threshold_weights = np.linspace(0, 1, 101)
+        # Iterate over the grid search parameters
+        if not include_threshold_optimization:
+            for weight in correlation_weights:
+                for num in num_features:
+                    loss = self.test_on_values(X_train, y_train, X_test, y_test, model, num, weight, loss_function)
+                    print(f"Weight: {weight}, Num features: {num}, Loss: {loss}")
+                    if loss < best_loss:
+                        best_loss = loss
+                        best_weight = weight
+                        best_num_features = num
+        else:
+            for weight in correlation_weights:
+                for num in num_features:
+                    for threshold_weight in threshold_weights:
+                        loss = self.test_on_values(X_train, y_train, X_test, y_test, model, num, weight, loss_function,
+                                                   threshold=threshold_weight)
+                        if loss == np.inf:
+                            print("No features selected, skipping")
+                            break
+                        print(f"Weight: {weight}, Num features: {num}, Score threshold: {threshold_weight}, Loss: {loss}")
+                        if loss < best_loss:
+                            best_loss = loss
+                            best_weight = weight
+                            best_num_features = num
+                            best_threshold = threshold_weight
+        # Set the best weight and number of features
+        self.correlation_weight = best_weight
+        # Get the selected features
+        print(f"Best weight: {best_weight}, Best loss: {best_loss}, Best num features: {best_num_features}")
+        X_transformed, selected_features, feature_scores = self.transform(X_train, num_features=best_num_features)
+        return X_transformed, selected_features, feature_scores, best_weight, best_loss, best_num_features, best_threshold
--- main_and_data/main.py
+++ main_and_data/main.py
@@ -3,6 +3,9 @@ import os
 
 from filter_methods import WeightedCombination
 from data_pre_processing import PreprocessingPipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.linear_model import LinearRegression
 
 # Get the directory of the currently running script
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,12 +20,45 @@ cols = ["OverallQual","GrLivArea","GarageCars","GarageArea","TotalBsmtSF","FullB
         "LotFrontage","MSSubClass", "SalePrice"]
 data = data[cols]
 # Select only the first 10 rows
-data = data.head(10)
 
-preprocessor = PreprocessingPipeline(data, ['MSSubClass'], 0.9)
+preprocessor = PreprocessingPipeline(data,
+                                     ['MSSubClass', 'FullBath'], 0.9)
 X, continuous_cols, cat_cols = preprocessor.preprocess()
 
+X_train, X_test, y_train, y_test = train_test_split(X, X['SalePrice'], test_size=0.2, random_state=42)
+
 filter_method = WeightedCombination()
-filter_method.fit(X, target_column="SalePrice", continuous_cols=continuous_cols, categorical_cols=cat_cols)
-X_transformed, selected_features, feature_scores = filter_method.transform(X, num_features=5)
+filter_method.fit(X_train, target_column="SalePrice", continuous_cols=continuous_cols, categorical_cols=cat_cols)
+# X_transformed, selected_features, feature_scores, best_weight, best_loss, best_num_features, best_threshold \
+#     = filter_method.auto_optimize(X_train,
+#                                   y_train, X_test,
+#                                   y_test,
+#                                   loss_function=mean_squared_error)
+filter_method.set_weight(0.3)
+X_transformed, selected_features, _ = filter_method.transform(X_train, num_features=13)
 print(selected_features)
+
+
+model = LinearRegression()
+model.fit(X_transformed, y_train)
+y_pred = model.predict(X_test[selected_features])
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+residuals = y_test - y_pred
+max_error = residuals.abs().max()
+mae = mean_absolute_error(y_test, y_pred)
+mape = mean_absolute_percentage_error(y_test, y_pred)
+print(f"With feature selection: MSE = {mse}, R2 = {r2}, max error = {max_error}, MAE = {mae}, MAPE = {mape}")
+
+X_train = X_train.drop(columns=['SalePrice'])
+X_test = X_test.drop(columns=['SalePrice'])
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+residuals = y_test - y_pred
+max_error = residuals.abs().max()
+mae = mean_absolute_error(y_test, y_pred)
+mape = mean_absolute_percentage_error(y_test, y_pred)
+print(f"Without feature selection: MSE = {mse}, R2 = {r2}, max error = {max_error}, MAE = {mae}, MAPE = {mape}")
