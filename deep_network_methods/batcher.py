41e35e805c90237f5d0ca1fa6f5a8d4db919cb76
Yuval Uner <yuvaluner@gmail.com>
2024-04-15 17:49:14 +0300

    Added batcher.py, made some fixes to my misconceptions when writing the environment and model.

---
+++ deep_network_methods/batcher.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+
+class Batcher:
+    """
+    A batcher for the reinforcement learning algorithms.
+    Cycle through the data and return a batch of data, always returning the same batch size.
+    """
+
+    def __init__(self, X, y, batch_size):
+        self.X = X
+        self.y = y
+        self.index = 0
+        self.prev_index = 0
+        self.batch_size = batch_size
+
+    def next_batch(self):
+        """
+        :return: The next batch of data
+        """
+        # Account for cases where the batch size does not divide the number of samples
+        # In this case, we loop back to the beginning of the data and append those samples to the batch
+        if self.index + self.batch_size > len(self.X):
+            batch_len = len(self.X) - self.index
+        else:
+            batch_len = self.batch_size
+        batch_X = self.X[self.index:self.index + batch_len]
+        batch_y = self.y[self.index:self.index + batch_len]
+        # Append the beginning of the data to the batch if necessary
+        if batch_len < self.batch_size:
+            batch_X = pd.concat([batch_X, self.X[:self.batch_size - batch_len]])
+            batch_y = pd.concat([batch_y, self.y[:self.batch_size - batch_len]])
+        # Update the index, ensuring that it wraps around when necessary
+        self.prev_index = self.index
+        self.index = (self.index + self.batch_size) % len(self.X)
+        return batch_X, batch_y
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next_batch()
+
+    def reset(self):
+        self.index = 0
+        self.prev_index = 0
+        return self.next_batch()
+
+    def is_done(self):
+        return self.index <= self.prev_index
--- deep_network_methods/fully_connected_selector.py
+++ deep_network_methods/fully_connected_selector.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import tensorflow as tf
-from keras import __version__
-tf.keras.__version__ = __version__
-from keras.layers import Dense
+from keras.layers import Dense, Flatten
 from keras.models import Sequential
 from .dataset_env import DatasetEnv
 from .agent_base import AgentBase
@@ -16,19 +13,17 @@ class FullyConnectedSelector(Sequential):
         self.targets = targets
         self.batch_size = batch_size
 
-        input_size = output_size = len(dataset.columns)
-        self.add(Dense(input_size, input_shape=(input_size,), activation='relu'))
+        input_size = len(dataset.columns) * batch_size
+        output_size = len(dataset.columns)
+        self.add(Flatten(input_shape=(batch_size, input_size,)))
+        self.add(Dense(input_size, activation='relu'))
         layer_size = input_size // 2
         while layer_size > output_size:
             self.add(Dense(layer_size, activation='relu'))
             layer_size = layer_size // 2
-        self.add(Dense(output_size, activation='sigmoid'))
+        self.add(Dense(output_size, activation='linear'))
         self.environment = DatasetEnv(dataset, targets, downstream_model, loss_function, batch_size, static_selection)
         self.agent = AgentBase(self, self.environment)
-
-    def __call__(self, *args, **kwargs):
-        output = super(FullyConnectedSelector, self).__call__(*args, **kwargs)
-        return tf.reshape(output, (self.batch_size, len(self.dataset.columns)))
 
     def train(self, nb_steps):
         self.agent.train(nb_steps)
